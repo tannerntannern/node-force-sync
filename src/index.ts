69450333f8c2d77329244238673a1c44aedeecd8
Tanner Nielsen <tannerntannern@gmail.com>
2020-08-22 01:06:46 -0500

    feat: error handling and config options

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,95 @@
 import { execSync } from 'child_process';
 import { writeFileSync, unlinkSync } from 'fs';
+import { resolve } from 'path';
+import escapeStringRegex from 'escape-string-regexp';
 
 type AsyncFunc = (...args: any[]) => Promise<any>;
 
 type UnwrapPromise<T extends Promise<any>> =
 	T extends Promise<infer V> ? V : unknown;
 
-export const forceSync = <F extends AsyncFunc>(asyncFunc: F) =>
-	(...args: Parameters<F>): UnwrapPromise<ReturnType<F>> => {
+export type ForceSyncConfig = {
+	tagOpenWrappers: [string, string],
+	tagCloseWrappers: [string, string],
+	tmpFilePath: string,
+	nodeExecutable: string,
+	debug: boolean,
+};
+
+export const forceSync = <F extends AsyncFunc>(asyncFunc: F, config?: Partial<ForceSyncConfig>) => {
+	const {
+		tagOpenWrappers,
+		tagCloseWrappers,
+		tmpFilePath,
+		nodeExecutable,
+		debug,
+	} = defaults<ForceSyncConfig>(config ?? {}, {
+		tagOpenWrappers: ['!!!', '!!!'],
+		tagCloseWrappers: ['!!!/', '!!!'],
+		tmpFilePath: '.',
+		nodeExecutable: 'node',  // TODO: make default platform dependent
+		debug: false,
+	});
+
+	const outputOpener = makeTag('OUTPUT', tagOpenWrappers);
+	const outputCloser = makeTag('OUTPUT', tagCloseWrappers);
+	const errorOpener = makeTag('ERROR', tagOpenWrappers);
+	const errorCloser = makeTag('ERROR', tagCloseWrappers);
+
+	return (...args: Parameters<F>): UnwrapPromise<ReturnType<F>> => {
 		const argsString = args.map(arg => JSON.stringify(arg)).join(', ');
-		const outputOpener = '!!!OUTPUT!!!';
-		const outputCloser = '!!!/OUTPUT!!!';
-		const codeString = `
-			const asyncFunc = ${asyncFunc.toString()};
-			asyncFunc(${argsString})
-				.then(output => console.log('${outputOpener}' + JSON.stringify(output) + '${outputCloser}'));
-		`;
+		const codeString =
+`(${Function.prototype.toString.call(asyncFunc)})(${argsString})
+	.then(function(output) {
+		console.log('${outputOpener}' + JSON.stringify(output) + '${outputCloser}');
+	})
+	.catch(function(error) {
+		var message = (error instanceof Error) ? error.message : error;
+		console.log('${errorOpener}' + JSON.stringify(message) + '${errorCloser}');
+	});`;
 
-		const tmpFilename = `tmp${Date.now()}.js`;
-		writeFileSync(tmpFilename, codeString, 'utf8');
-		const rawOutput = execSync(`node ${tmpFilename}`).toString();
-		unlinkSync(tmpFilename);
+		const tmpFile = resolve(tmpFilePath, `tmp${Date.now()}.js`);
+		if (debug) {
+			console.info('Generated JS:');
+			console.log(codeString);
+			console.info('Temporary file path:');
+			console.log(tmpFile);
+		}
 
-		// TODO: need a better regex matcher
-		const output = new RegExp(`${outputOpener}(.+)${outputCloser}`).exec(rawOutput)?.[1] ?? '';
+		writeFileSync(tmpFile, codeString, 'utf8');
+		const rawOutput = execSync(`${nodeExecutable} ${tmpFile}`).toString();
+		unlinkSync(tmpFile);
 
-		return JSON.parse(output) as any;
+		if (debug) {
+			console.info('Execution output:');
+			console.log(rawOutput);
+		}
+
+		const output = extractOutput(rawOutput, outputOpener, outputCloser);
+		const error = extractOutput(rawOutput, errorOpener, errorCloser);
+
+		if (debug) {
+			console.info('Extracted output:');
+			console.log(output);
+			console.info('Extracted error:');
+			console.log(error);
+		}
+
+		const isError = error !== null || (output === null && error === null);
+		if (isError)
+			throw new Error('' + error);
+
+		return JSON.parse(output as string) as any;
 	};
+};
+
+const extractOutput = (rawOutput: string, openTag: string, closeTag: string) =>
+	new RegExp(`${escapeStringRegex(openTag)}([\\s\\S]+)${escapeStringRegex(closeTag)}`).exec(rawOutput)?.[1] ?? null;
+
+const makeTag = (middle: string, [beginning, ending]: [string, string]) =>
+	beginning + middle + ending;
+
+const defaults = <T>(config: Partial<T>, defaults: T): T =>
+	Object.assign({}, defaults, config);
 
 export default forceSync;
